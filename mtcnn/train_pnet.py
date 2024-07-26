f3cdf4cb39c90bce926bb5141d8b03b0a3e4498f
jhzhu <jhzhu@telenavsoftware.com>
2024-07-26 11:39:40 +0800

    P-Net

--- mtcnn/train_pnet.py
+++ mtcnn/train_pnet.py
@@ -1,21 +1,25 @@
+import time
+
 import nets
 import os
 import train
 
 if __name__ == '__main__':
     # 权重存放地址
-    param_path = r"param1/p_net.pt"
+    param_path = r"param_before/p_net.pt"
     # 数据存放地址
-    data_path = r"test_data/MTCNN/12"
+    data_path = r"train_data/MTCNN/12"
     
     # 如果没有这个参数存放目录，则创建一个目录
-    if not os.path.exists("param1"):
-        os.makedirs("param1")
+    if not os.path.exists("param_before"):
+        os.makedirs("param_before")
 
     # 构建模型
     pnet = nets.PNet()
-
+    begin = time.time()
     # 开始训练
     t = train.Trainer(pnet, param_path, data_path)
 
     t.train(0.01)
+    end = time.time()
+    print('PNet training time %s' % (end - begin))
